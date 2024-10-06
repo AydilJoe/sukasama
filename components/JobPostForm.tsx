9e1df9ebbf8363835e6d16b4a58dcc580f655860
AydilJoe <aydiljohari@gmail.com>
2024-10-06 12:06:48 +0800

    Update JobPostForm.tsx

--- components/JobPostForm.tsx
+++ components/JobPostForm.tsx
@@ -207,6 +207,7 @@ export default function JobPostForm({ onPostCreated }: JobPostFormProps) {
                   placeholder="Type to search job name"
                   bg={inputBgColor}
                   size="md"
+                  autocomplete="on"
                 />
                 <InputRightElement>
                   <ChevronDownIcon />
