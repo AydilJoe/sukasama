e49c7628da6a66892e676a8c95d833cb73fa66ca
AydilJoe <aydiljohari@gmail.com>
2024-10-23 23:53:10 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
             <head>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1513356284203988"
-     crossOrigin="anonymous"></script>
+            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1513356284203988"
+     crossOrigin="anonymous"/>
       </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
